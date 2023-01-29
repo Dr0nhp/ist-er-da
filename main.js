5ca6b84cbbbd6a8b9a9a05724cfdff29732f5a1a
Dr0nhp <13246131+Dr0nhp@users.noreply.github.com>
2023-01-29 23:30:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 function generateDays() {
-    /*
-    0 = Not available
-    1 = save
-    2 = maybe
-    3 = raid
-    */
-    const where = [0, 3, 1, 2, 3, 2, 2]
+    const where = [0, 0, 1, 1, 0, 0, 1]
     const d = new Date();
     let cur_day = d.getDay();
     return [cur_day, where]
@@ -22,27 +16,25 @@ function a1(cur_day, where) {
 }
 
 function a2(cur_day, where) {
-    if (where[cur_day] != 1) { //mod7
+    if (where[cur_day] != 1) {
         let c = []; //alle save dates als array pos
-        for (let i = 0; i < where.length - 1; i++) {
+        for (let i = 0; i < where.length; i++) {
             if (where[i] == 1) {
                 c.push(i);
             }
         }
 
         if (c.length < 1) {
-            console.log("error keine save dates");
+            return 9999;
         }
         else if (c.length == 1) {
             return (c[0] - cur_day % 7);
         }
         else {
-            console.log(c)
             let dif = [];
             for (e in c) {
-                dif.push(e - cur_day % 7);
+                dif.push(c[e] - cur_day % 7);
             }
-            console.log(dif)
             return (dif.sort()[0]);
         }
 
