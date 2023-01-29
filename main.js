0640bacf622e587b2a519dbb6d870d4d6e5cbf55
Dr0nhp <13246131+Dr0nhp@users.noreply.github.com>
2023-01-29 23:01:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,22 +13,23 @@ function generateDays() {
 
 
 function a1(cur_day, where) {
-    if (where[cur_day != 1]) {
-        return "no";
+    if (where[cur_day] != 1) {
+        return "Sorry, nein";
     }
     else {
-        return "yes";
+        return "Jawollo, BR BR BR";
     }
 }
 
 function a2(cur_day, where) {
-    if (where[cur_day != 1]) { //mod7
-        let c; //alle save dates als array pos
+    if (where[cur_day] != 1) { //mod7
+        let c = []; //alle save dates als array pos
         for (let i = 0; i < where.length - 1; i++) {
-            if (where[i] = 1) {
+            if (where[i] == 1) {
                 c.push(i);
             }
         }
+
         if (c.length < 1) {
             console.log("error keine save dates");
         }
@@ -36,12 +37,15 @@ function a2(cur_day, where) {
             return (c[0] - cur_day % 7);
         }
         else {
-            let dif = 0;
+            console.log(c)
+            let dif = [];
             for (e in c) {
                 dif.push(e - cur_day % 7);
             }
+            console.log(dif)
             return (dif.sort()[0]);
         }
+
     }
     else {
         return 0; //0 Tage
@@ -52,8 +56,7 @@ function main() {
     const helper = generateDays()
     const a = helper[0];
     const b = helper[1];
-    document.getElementById("answer-1").textContent(a1(a, b))
+    document.getElementById("answer-1").innerHTML = a1(a, b);
+    document.getElementById("answer-2").innerHTML = a2(a, b);
 }
-
-
-document.onload(main());
+main();
