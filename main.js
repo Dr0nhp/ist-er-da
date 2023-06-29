868ed7243d8a93964501047da7f7d3d02ef66fec
Daniel Mitesser <daniel.mitesser@netgrade.de>
2023-06-29 15:03:07 +0200

    code reviewd

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 function generateDays() {
-    const where = [0, 3, 1, 2, 3, 2, 2]
-    const d = new Date();
-    let cur_day = d.getDay();
-    return [cur_day, where]
+    const timeTable = [0, 3, 1, 2, 3, 2, 2]
+    let currentDay = new Date().getDay();
+    return [currentDay, timeTable]
 }
 
 
-function a1(cur_day, where) {
-    if (where[cur_day] != 1) {
+function hereToday(currentDay, timeTable) {
+    if (timeTable[currentDay] != 1) {
         return "Sorry, nein";
     }
     else {
@@ -15,41 +14,27 @@ function a1(cur_day, where) {
     }
 }
 
-function a2(cur_day, where) {
-    if (where[cur_day] != 1) {
-        let c = []; //alle save dates als array pos
-        for (let i = 0; i < where.length; i++) {
-            if (where[i] == 1) {
-                c.push(i);
-            }
-        }
-
-        if (c.length < 1) {
-            return 9999;
-        }
-        else if (c.length == 1) {
-            console.log(c[0],cur_day);
-            return ((c[0] - cur_day) % 7);
-        }
-        else {
-            let dif = [];
-            for (e in c) {
-                dif.push((c[e] - cur_day) % 7);
-            }
-            return (dif.sort()[0]);
-        }
-
+function nextTíme(currentDay, timeTable) {
+    const daysWithSchedule = timeTable.reduce((accumulator, value, index) => {
+      if (value === 1) {
+        accumulator.push(index);
+      }
+      return accumulator;
+    }, []);
+  
+    if (daysWithSchedule.length < 1) {
+      return 9999;
+    } else if (daysWithSchedule.length === 1) {
+      return (daysWithSchedule[0] - currentDay) % 7;
+    } else {
+      const dayDifferences = daysWithSchedule.map((day) => (day - currentDay) % 7);
+      return Math.min(...dayDifferences);
     }
-    else {
-        return 0; //0 Tage
-    }
-}
+  }
 
 function main() {
     const helper = generateDays()
-    const a = helper[0];
-    const b = helper[1];
-    document.getElementById("answer-1").innerHTML = a1(a, b);
-    document.getElementById("answer-2").innerHTML = a2(a, b);
+    document.getElementById("answer-1").innerHTML = hereToday(currentDay, timeTable);
+    document.getElementById("answer-2").innerHTML = nextTíme(currentDay, timeTable);
 }
 main();
